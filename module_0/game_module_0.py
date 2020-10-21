2e943f0b72dbd66c51ac2189461ab068fe78a736
Evgenii Bistrov <ebistrov@gmail.com>
2020-10-21 12:18:34 +0600

    push py game

---
+++ module_0/game_module_0.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+
+def score_game(game_core):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра
+    угадывает число'''
+    count_ls = []
+    np.random.seed(1)
+    random_array = np.random.randint(1, 101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return score
+
+
+def game_core_v1(number):
+    '''Просто угадываем на random, никак не используя информацию о больше или
+        меньше. Функция принимает загаданное число и возвращает число
+        попыток'''
+    count = 0
+    while True:
+        count += 1
+        predict = np.random.randint(1, 101)
+        if number == predict:
+            return count
+
+
+def game_core_v2(number):
+    '''Сначала устанавливаем любое random число, а потом уменьшаем или
+        увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 1
+    predict = np.random.randint(1, 101)
+    while number != predict:
+        count += 1
+        if number > predict:
+            predict += 1
+        elif number < predict:
+            predict -= 1
+    return count
+
+
+def game_core_v3(number):
+    '''Сначала устанавливаем медиану возможных чисел(predict), а потом
+        уменьшаем или увеличиваем его в зависимости от того, больше оно или
+        меньше нужного с шагом(predict_step), который уменьшается в 2 раза за
+        каждую итерацию. Проще говоря, реализуется алгоритм поиска значения
+        методом деления отрезка пополам.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 1
+    predict = 50
+    predict_step = predict // 2
+    while number != predict:
+        count += 1
+        if number > predict:
+            predict += predict_step
+        elif number < predict:
+            predict -= predict_step
+        if predict_step > 1:
+            predict_step //= 2
+    return count
+
+# Тест первой версии
+score_game(game_core_v1)
+# Тест второй версии
+score_game(game_core_v2)
+# Тест моей версии
+score_game(game_core_v3)
